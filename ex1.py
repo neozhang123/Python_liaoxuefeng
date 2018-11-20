de70896844bf825d888db38db73e03c1b2ea6db3
neozhang123 <45189271+neozhang123@users.noreply.github.com>
2018-11-20 15:16:35 +0800

    Delete ex1.py

--- ex1.py
+++
@@ -1,9 +0,0 @@
-print ('zhangxiaoyi', 'shige dahuaidan')
-print (300, 'lalala', 300 + 200)
-
-name = input('Please input your name: ')
-print ('Hello,', name)
-
-#这是练习
-print ('1024 * 768 =', 1024 * 768)
-
